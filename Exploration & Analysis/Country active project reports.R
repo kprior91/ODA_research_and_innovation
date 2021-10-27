fe7a4e9314b00e8fb4f8d4fc9e147df279fdc010
Emma Clegg <e-clegg@dfid.gov.uk>
2021-10-27 13:00:01 +0100

    Country reports - formatting added

--- Exploration & Analysis/Country active project reports.R
+++ Exploration & Analysis/Country active project reports.R
@@ -16,6 +16,17 @@ all_projects_tidied <- readRDS("Outputs/all_projects_tidied.rds")
 
 wb <- openxlsx::createWorkbook()
 
+# Set header style (bold)
+header_st <- createStyle(textDecoration = "Bold",
+                         fontSize = 8, fontName = "Arial",
+                         halign = "center")
+
+# Set table style
+table_st <- createStyle(fontSize = 8, fontName = "Arial", 
+                        wrapText = TRUE,
+                        valign = "top")
+
+
 for(i in 1:length(country_list)) {
 
     print(paste0(i, " - ", country_list[i]))
@@ -38,14 +49,22 @@ for(i in 1:length(country_list)) {
                Funder == "Department of Health and Social Care" ~ "DHSC",
                Funder == "Department for Business, Energy and Industrial Strategy" ~ "BEIS"
              ),
-             link = coalesce(link, "https://devtracker.fcdo.gov.uk")) %>% 
-      select(Funder, Fund, Title = title, Start, End, Description = abstract,
+             Fund = if_else(str_detect(Fund, "FCDO Research & Innovation|FCDO fully"), "FCDO Research - Programmes", Fund),
+             Fund = if_else(str_detect(Fund, "FCDO partially"), "FCDO Research - Partnerships", Fund),
+             link = coalesce(link, "")) %>% 
+      select(Funder, Fund, 
+             Title = title, Start, End, Description = abstract,
              `Lead Organisation` = lead_org_name, `Partner Organisations` = partner_org_name,
              `Web Link` = link) %>% 
       group_by(Title, Funder) %>% 
       slice(1) %>% 
       ungroup() %>% 
       unique()
+    
+    # Join on funder programme names
+    output_report <- output_report %>% 
+      left_join(select(all_projects_tidied, title, funder_programme) %>% unique(), by = c("Title" = "title")) %>% 
+      rename(Programme = funder_programme)
     
     # Summarise funders on co-funded projects
     co_funded_projects <- output_report %>% 
@@ -59,31 +78,34 @@ for(i in 1:length(country_list)) {
     output_report <- output_report %>% 
       left_join(co_funded_projects, by = "Title") %>% 
       mutate(Funder = coalesce(comb_funder, Funder)) %>% 
-      select(-comb_funder) %>% 
       group_by(Title) %>% 
       # Keep one row per project title
-      slice(1)
+      slice(1) %>% 
+      select(Funder, Fund, Programme, Title, Start, End, Description, `Lead Organisation`, `Partner Organisations`, `Web Link`)
   
     # Add country dataset to output list
     openxlsx::addWorksheet(wb, sheetName = country_list[i])
-    openxlsx::writeData(wb, sheet = i, x = output_report)
+    openxlsx::writeData(wb, sheet = i, x = output_report, 
+                        headerStyle = header_st,
+                        borderStyle = "thin")
+    # Add font style
+    addStyle(wb, sheet = i, table_st, rows = 2:200, cols = 1:10, gridExpand = TRUE, stack = TRUE)
     
     # Identify titles and hyperlinks
     hyperlinks <- output_report$`Web Link`
-    names(hyperlinks) <- output_report$Title
+    names(hyperlinks) <- output_report$`Web Link`
     class(hyperlinks) <- "hyperlink"
     
     # Write hyperlinks
-    writeData(wb, sheet = i, x = hyperlinks, startRow = 2, startCol = 3, colNames = FALSE)
+    writeData(wb, sheet = i, x = hyperlinks, startRow = 2, startCol = 10, colNames = FALSE)
     
     # Set column widths
-    setColWidths(wb, sheet = i, cols = 1:8, widths = c(8,25,40,6,6,40,30,30))
+    setColWidths(wb, sheet = i, cols = 1:10, widths = c(10,25,30,40,6,6,60,30,30,50))
     
 }
 
 # Resave Excel file
 saveWorkbook(wb, "Outputs//East Africa ODA programmes - Oct21.xlsx", overwrite = TRUE)
-
 
 
 
